c83932dd741df9a9a9ccc5405eadbbe64f1055aa
Zachary Fowler <fowlerzw@vcu.edu>
2021-12-05 20:20:53 -0500

    commit with ui

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/add')
+def add():  # put application's code here
+    return 'Add'
+
+@app.route('/delete')
+def delete():  # put application's code here
+    return 'Delete'
+
+@app.route('/logout')
+def logout():  # put application's code here
+    return 'You have been logged out'
 
 @app.route('/')
-def main():  # put application's code here
-    return '413 Project'
+@app.route('/login')
+def login():  # put application's code here
+    return render_template('login.html')
+
+@app.route('/home')
+def home():  # put application's code here
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
