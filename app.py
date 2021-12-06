2d4ac7523c41dc24d0fb328fb088aa7433a08f2f
Zachary Fowler <fowlerzw@vcu.edu>
2021-12-06 13:00:53 -0500

    commit with some updated styling to UI, still needs backend capability to check valid password and the add and delete entries. Also needs work with the data table as the dat currently is hardcoded

--- app.py
+++ app.py
@@ -1,27 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 
 app = Flask(__name__)
 
-@app.route('/add')
-def add():  # put application's code here
-    return 'Add'
-
-@app.route('/delete')
-def delete():  # put application's code here
-    return 'Delete'
-
+#this renders the logout template
 @app.route('/logout')
 def logout():  # put application's code here
-    return 'You have been logged out'
+    return render_template('logout.html')
 
+#this renders logout to starting endpoint and /login with login template
 @app.route('/')
 @app.route('/login')
 def login():  # put application's code here
     return render_template('login.html')
 
+# This renders the main template with password data table
 @app.route('/home')
-def home():  # put application's code here
+def home():
     return render_template('home.html')
+
+# This checks if the masterkey is correct
+@app.route('/getPassword')
+def getPassword():
+    #fill with code to save masterkey
+    if request.method == 'POST':
+        password = request.form['password']
+
+        #stores current user password
+        session['password'] = password
+
+        #check if password is correct
+
+        #redirects to home
+        return redirect('home')
+
+
+# This adds a new entry to the data table
+@app.route('/addPassword')
+def addPassword():
+    #fill with code to add a new password and domain
+    if request.method == 'POST':
+        domainname = request.form['domainname']
+        password = request.form['password']
+
+        # stores current user password
+        session['domainname'] = domainname
+        session['password'] = password
+
+        # adds to the data table
+
+        # redirects to home (unneeded)
+        return redirect('home')
+
+# This deletes the inputted entry from the data table
+@app.route('/deletePassword')
+def deletePassword():
+    #fill with code to delete entry
+    if request.method == 'POST':
+        domainname = request.form['domainname']
+        password = request.form['password']
+
+        # stores current user password
+        session['domainname'] = domainname
+        session['password'] = password
+
+        # Deletes from the data table
+
+        # redirects to home (unneeded)
+        return redirect('home')
 
 
 if __name__ == '__main__':
