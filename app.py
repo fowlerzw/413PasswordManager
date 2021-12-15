2cd0eb2b19ae0172fbd53519974ccbeba3dc87e6
1-61Kilometers <milespopiela@gmail.com>
2021-12-15 14:45:07 -0500

    Comments
    Comments

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ from cryptography.fernet import Fernet
 
 
 
-pee = b'kh8n73vwtCY77uC7UGI6Lyqryw_1lpirSs9lqIaFHJY='
+
 f = Fernet(pee)
+
+#Hard coded hash key
+pee = b'kh8n73vwtCY77uC7UGI6Lyqryw_1lpirSs9lqIaFHJY='
 key = "$pbkdf2-sha256$29000$lDKm1BrjHIOQshaidC4FYA$5XCeCdY9e0K7yzsMaVstl65KisBvotxOdNuh00zFvsc"
 filename = "DataSet.csv"
 
-
+#Flask API
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c676dfde28b0b13ce00ba2455791628b'
 
@@ -37,11 +40,14 @@ def home():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     else:
+        #reads csv file
         df = pd.read_csv(filename, names=["site", "user", "pass"], encoding='unicode_escape')
+        #loop through password column and decrpyt
         for passwords in df["pass"]:
             names = decrypt(passwords)
             df["pass"].replace({passwords: names}, inplace=True)
 
+        #send html elements
         result = df.to_html()
         return render_template('home.html', result=result)
 
@@ -87,15 +93,13 @@ def addPassword():
 
 #encrypt
 def encrypt(password):
-  
-
     password = password.encode()
     encryptpass = f.encrypt(password)
     return encryptpass
 
 #decrypt
 def decrypt(encryptpass):
-
+    #decrypt using hash
     encryptpass = encryptpass[2:-1]
     passw = encryptpass.encode()
     decryptedpass = f.decrypt(passw)
@@ -107,10 +111,10 @@ def decrypt(encryptpass):
 def deletePassword():
     #fill with code to delete entry
     if request.method == 'POST':
+        #deletes by chosen row
         domainname = request.form['rowNum']
         df = pd.read_csv(filename, names=["site","user","pass"])
         df.drop(int(domainname),0,inplace=True)
-        
         df.to_csv(filename,header=False,index=False)
         return redirect('home')
 
